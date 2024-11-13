c0eaa7c988ec901696762ff74c86d76ebda00441
saritadipa34 <saritadipa34@gmail.com>
2024-11-13 13:53:12 +0530

    Header created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 import styles from "./header.module.css";
 
 const Header = () => {
   return (
-    <div className={styles.Header}
+    <div className={`${styles.header} container`}
      >
       <div><img src="../src/assets/images/logo.png" alt="pic" /></div>
       <ul>
